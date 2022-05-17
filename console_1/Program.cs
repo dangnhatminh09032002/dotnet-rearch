c87c13db70dbce48eb89d0b183864d0205663c61
dangnhatminh <dangnhatminh09032002@gmail.com>
2022-05-17 21:05:31 +0700

    delegie

--- console_1/Program.cs
+++ console_1/Program.cs
@@ -9,17 +9,28 @@ namespace Global
 
     class Program
     {
-        static public void Info(string message)
+        static public void InfoSuccess(string message)
         {
+            ForegroundColor = ConsoleColor.Green;
             WriteLine(message);
+            ResetColor();
+        }
+
+        static public void InfoWarning(string message)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(message);
+            ResetColor();
         }
         static void Main(string[] args)
         {
             string message = "HELLO";
-            ShowMessage show = null;
-            show = Info;
-            show.Invoke("Show");
-            Info(message);
+            ShowMessage show;
+            show = InfoSuccess;
+            show += InfoWarning;
+            show += InfoWarning;
+            show += InfoSuccess;
+            show?.Invoke(message);
         }
     }
 }
