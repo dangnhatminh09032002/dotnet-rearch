532aafa19bcc5b3e87961a79f9fb0c3636108a14
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-17 09:25:15 +0700

    CS02: Bien, Hang,...

---
+++ console_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            string address;
+            double score;
+            Console.WriteLine("Your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Your age: ");
+            age = Console.Read();
+
+            Console.WriteLine("Name: " + name + "\n" + "Age: " + age + "\n");
+        }
+    }
+}
