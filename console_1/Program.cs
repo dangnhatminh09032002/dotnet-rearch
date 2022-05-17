c8aad3a290e5bcbcae9be7368a84adf4c0df922a
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-17 15:24:12 +0700

    null and nullable

--- console_1/Program.cs
+++ console_1/Program.cs
@@ -8,28 +8,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Anonymous types
-        List<User> listUsers = new List<User>() {
-            new User(){Name = "John", Age = 12},
-            new User(){Name = "Trinh", Age = 13},
-            new User(){Name = "Chi", Age = 14},
-            new User(){Name = "Tram", Age = 15},
-
-        };
-
-        var usersSelected = from user in listUsers
-                            where user.Age >= 12
-                            select user;
-
-        foreach (var user in usersSelected)
-        {
-            WriteLine(user.Name, user.Age);
-
-        }
-
-        // Dynamic types
-        dynamic obj = new Object();
-        obj.usres = listUsers;
-        obj = "";
+        int? number;
+        number = null;
+        WriteLine("number: " + number);
     }
 }
