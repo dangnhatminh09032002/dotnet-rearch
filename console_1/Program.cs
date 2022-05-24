93a76deeac60d1e3d7afd1039669398bf6af195c
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-24 10:35:09 +0700

    cs22 Event pp3

--- console_1/Program.cs
+++ console_1/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
+
+class DataInputs : EventArgs
+{
+    public int Data { set; get; }
+    public DataInputs(int x)
+    {
+        Data = x;
+    }
+}
+
 class Program
 {
     /* 
         publisher -> class - phat su kien
         subsriber -> class - nhan su kien
     */
-
-    public delegate void InputNumberEvent(int x);
+    public delegate void InputNumberEvent(object? sender, DataInputs args);
     // Publisher
     class UserInput
     {
-        public event InputNumberEvent inputNumberEvent; // Phuong thuc nay khong the bi gan lai chi co the them sua xoa
+        // public event InputNumberEvent inputNumberEvent; // Phuong thuc nay khong the bi gan lai chi co the them sua xoa
+
+        public event InputNumberEvent inputNumberEvent; // ~ Deleget void KIEU(object? sender, EventArgs args)
+
         public void Input()
         {
             do
@@ -20,7 +32,7 @@ class Program
                 try
                 {
                     int i = Int32.Parse(s);
-                    inputNumberEvent?.Invoke(i);
+                    inputNumberEvent?.Invoke(this, new DataInputs(i));
                 }
                 catch (Exception ex) { }
             } while (true);
@@ -32,18 +44,20 @@ class Program
         }
     }
 
-    class MyEvent
-    {
-        public static void SubEvent(UserInput input, InputNumberEvent callback)
-        {
-            input.inputNumberEvent += callback;
-        }
-    }
+    // class MyEvent
+    // {
+    //     public static void SubEvent(UserInput input, InputNumberEvent callback)
+    //     {
+    //         input.inputNumberEvent += callback;
+    //     }
+    // }
     public static void Main(string[] args)
     {
         UserInput userInput = new UserInput();
-        userInput.SubEvent(void (int x) => Console.WriteLine($"Binh phuong cua {x} la: " + x * x));
-        userInput.SubEvent(void (int x) => Console.WriteLine($"Gap doi cua {x} la: " + x * x));
+        userInput.SubEvent(void (object? sender, DataInputs data) =>
+        {
+            Console.WriteLine($"Binh phuong cua {data.Data} la: " + data.Data * data.Data);
+        });
 
         // MyEvent.SubEvent(userInput, void (int x) => Console.WriteLine($"Binh phuong cua {x} la: " + x * x));
         // MyEvent.SubEvent(userInput, void (int x) => Console.WriteLine($"Gap doi cua {x} la: " + x * x));
