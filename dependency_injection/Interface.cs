f7185bb94ec622995c5762d094af9b6cbfc8e67e
dangnhatminh <dangnhatminh09032002@gmail.com>
2022-05-25 23:07:02 +0700

    CS33 using dotnet new mvc

--- dependency_injection/Interface.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Minh.DependencyInjection.Interfaces
-{
-    public interface IMyService { }
-    public interface IMyServiceOptions
-    {
-        public string Name { get; set; }
-    }
-}
