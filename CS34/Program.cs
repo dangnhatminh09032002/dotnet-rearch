19a8bd5ca9e8a62c7299073d19723cdb4c4ad625
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-27 14:56:33 +0700

    CS35 MultipartFormDataContent

--- CS34/Program.cs
+++ CS34/Program.cs
@@ -4,109 +4,36 @@ using System.Reflection;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string host = "www.facebook.com";
-        string url = $"https://{host}";
-
-        byte[] buffer = await DownloadPageDataBytes(url);
-
-        Task task_3 = SaveFileInHereWithBytes(buffer, "facebook.html");
-        DownloadStream(url, "facebook.html");
-
-        Console.ReadKey();
-    }
-
-
-    static async Task<string> GetWebContent(string url)
-    {
-        try
-        {
-            using HttpClient httpClient = new HttpClient();
-
-            // Add headers before request
-            httpClient.DefaultRequestHeaders.Add("content-type", "application/json");
-
-            // Send request
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
-            //ShowHeader(httpResponseMessage.Headers);
-            return await httpResponseMessage.Content.ReadAsStringAsync();
-        }
-        catch (Exception ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Page not found");
-            Console.ResetColor();
-            return "";
-        }
-    }
-    static async Task<byte[]> DownloadPageDataBytes(string url)
-    {
         using HttpClient httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
 
-        byte[] buffer = new byte[] { };
-        try
-        {
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
-            byte[] bytes = await httpResponseMessage.Content.ReadAsByteArrayAsync();
-        }
-        catch (Exception ex) { }
-        return buffer;
-    }
-    static async Task DownloadStream(string url, string fileName)
-    {
-        HttpClient httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
-        string path = Path.GetFullPath(fileName); // Path to the file
-        try
-        {
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
-            using Stream stream = await httpResponseMessage.Content.ReadAsStreamAsync();
-            using FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-            // Doan code ben duoi se save file theo tung EVERY_SIZE cho den khi full
-            const int EVERY_SIZE = 1000; // save other 1000 byte
-            do
-            {
-                byte[] buffer = new byte[EVERY_SIZE];
-                int sizeByteCurrent = await stream.ReadAsync(buffer, 0, buffer.Length);
-                fsWrite.Write(buffer, 0, sizeByteCurrent);
-                if (sizeByteCurrent == 0) break;
-            } while (true);
-        }
-        catch (Exception ex) { }
-    }
-    static async Task SaveFileInHereWithBytes(byte[] buffer, string fileName)
-    {
-        // Save data to file
-        string path = Path.GetFullPath(fileName);
-        using FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
-        fs.Write(buffer, 0, buffer.Length);
-    }
-    static void ShowHeader(HttpResponseHeaders headers)
-    {
-        Console.WriteLine($"-- Headers --");
-        foreach (var header in headers)
-        {
-            Console.WriteLine($"{header.Key} : {header.Value}");
-        }
+
+        // string data = @"{
+        //     ""name"": ""dang nhat minh"",
+        //     ""age"": ""20""
+        // }";
+
+        //FormUrlEncodedContent content = new FormUrlEncodedContent(new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("name", "dang nhat minh") });
+        // StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+        MultipartFormDataContent content = new MultipartFormDataContent();
+        content.Add();
+        HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+        httpRequestMessage.Content = content;
+        httpRequestMessage.Method = HttpMethod.Post;
+        // httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+        //httpRequestMessage.Content = new StringContent(Newtonsoft.Json.JsonSerializer(new {}), Encoding.UTF8, "application/json");
+        httpRequestMessage.RequestUri = new Uri("https://www.google.com");
+        httpRequestMessage.Headers.Add("User-Agent", "Mozilla/5.0");
+
+        HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+        string html = await httpResponseMessage.Content.ReadAsStringAsync();
+        Console.WriteLine(httpRequestMessage.Headers.ToString());
     }
 }
-
-// var ping = new Ping();
-// var response = ping.Send(host);
-
-// if (response.Status == IPStatus.Success)
-// {
-//     Uri uri = new Uri(url);
-//     PropertyInfo[] properties = uri.GetType().GetProperties();
-//     foreach (PropertyInfo property in properties)
-//     {
-//         Console.WriteLine($"{property.Name}: {property.GetValue(uri)}");
-//     }
-// }
-// Task<string> task = GetWebContent(url);
-// task.Wait();
