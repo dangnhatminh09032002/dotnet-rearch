5ab86cd1218da4bd91f6bc2a3226c6d38aa40103
dangnhatminh <dangnhatminh09032002@gmail.com>
2022-05-29 02:35:17 +0700

    cs37 test package Npgsql

--- cs37/Program.cs
+++ cs37/Program.cs
@@ -1,7 +1,40 @@
-﻿namespace Minh.Cs37
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Npgsql;
+
+namespace Minh.Cs37
 {
     public class Program
     {
-        public static void Main(string[] args) { }
+        public static async Task Main(string[] args)
+        {
+            // Config
+            ConfigurationBuilder configarationBuilder = new ConfigurationBuilder();
+            configarationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configarationBuilder.AddJsonFile("config.json");
+
+            ConfigurationRoot configRoot = configarationBuilder.Build() as ConfigurationRoot;
+            string connectString = configRoot["Database:PostgreSQL:ConnectString:HangfireConnection"];
+
+            // Connect to database
+            await using NpgsqlConnection conn = new NpgsqlConnection(connectString);
+            await conn.OpenAsync();
+
+            await using NpgsqlCommand sqlCommand = new NpgsqlCommand("SELECT * FROM persion", conn);
+            await using (NpgsqlDataReader reader = await sqlCommand.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    byte buffer = reader.GetByte(0);
+                    Console.WriteLine(buffer);
+                };
+            };
+        }
+    }
+    class Persion
+    {
+        public int id;
+        public string name;
+        public int age;
     }
 }
