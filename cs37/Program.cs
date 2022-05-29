a8262d32e4190e28b0fe371e27bc09dec1172ef4
dangnhatminh <dangnhatminh09032002@gmail.com>
2022-05-29 20:15:32 +0700

    cs37

--- cs37/Program.cs
+++ cs37/Program.cs
@@ -1,40 +1,24 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using Npgsql;
 
 namespace Minh.Cs37
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            // Config
-            ConfigurationBuilder configarationBuilder = new ConfigurationBuilder();
-            configarationBuilder.SetBasePath(Directory.GetCurrentDirectory());
-            configarationBuilder.AddJsonFile("config.json");
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory()); // Cần có Microsoft.Extensions.Configuration.Json để dùng module này;
+            configurationBuilder.AddJsonFile("config.json");
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+            string connectionString = configurationRoot["Database:PostgreSql:ConnectString:HangfireConnection"];
 
-            ConfigurationRoot configRoot = configarationBuilder.Build() as ConfigurationRoot;
-            string connectString = configRoot["Database:PostgreSQL:ConnectString:HangfireConnection"];
-
-            // Connect to database
-            await using NpgsqlConnection conn = new NpgsqlConnection(connectString);
-            await conn.OpenAsync();
-
-            await using NpgsqlCommand sqlCommand = new NpgsqlCommand("SELECT * FROM persion", conn);
-            await using (NpgsqlDataReader reader = await sqlCommand.ExecuteReaderAsync())
-            {
-                while (await reader.ReadAsync())
-                {
-                    byte buffer = reader.GetByte(0);
-                    Console.WriteLine(buffer);
-                };
-            };
+            //Connect to database
+            // SqlConnection connection = new SqlConnection(connectionString);
+            // Console.WriteLine(connection.State);
+            // connection.Open();
+            // Console.WriteLine(connection.State);
+            // connection.Close();
         }
-    }
-    class Persion
-    {
-        public int id;
-        public string name;
-        public int age;
     }
 }
