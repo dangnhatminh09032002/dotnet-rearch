44c70654fdbfd60c23ef64cdbbde3a654b1ddede
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-30 11:01:19 +0700

    cs37 sqlcommand

--- cs37/Program.cs
+++ cs37/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Npgsql;
+using System.Data.SqlClient;
 
 namespace Minh.Cs37
 {
@@ -11,14 +11,29 @@ namespace Minh.Cs37
             configurationBuilder.SetBasePath(Directory.GetCurrentDirectory()); // Cần có Microsoft.Extensions.Configuration.Json để dùng module này;
             configurationBuilder.AddJsonFile("config.json");
             IConfigurationRoot configurationRoot = configurationBuilder.Build();
-            string connectionString = configurationRoot["Database:PostgreSql:ConnectString:HangfireConnection"];
+            string connectionString = configurationRoot["Database:SqlServer:ConnectString:HangfireConnection"];
+
+            // SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            // sqlConnectionStringBuilder.Add("Server", "localhost");
+            // SqlConnectionStringBuilder.Add("Port", "1433");
+            // sqlConnectionStringBuilder.Add("Database", "dotnet_rearch");
+            // sqlConnectionStringBuilder.Add("UID", "sa");
+            // sqlConnectionStringBuilder.Add("PWD", "Nhtminh09032002");
+            // connectionString = sqlConnectionStringBuilder.ToString();
 
             //Connect to database
-            // SqlConnection connection = new SqlConnection(connectionString);
-            // Console.WriteLine(connection.State);
-            // connection.Open();
-            // Console.WriteLine(connection.State);
-            // connection.Close();
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            //Query the database
+            using SqlCommand command = new SqlCommand("SELECT TOP 20 * FROM persion", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"----------\nid: {reader["id"], 3}\nname: {reader["name"],10}\nage: {reader["age"],3}");
+            }
+
+            connection.Close();
         }
     }
 }
