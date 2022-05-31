7b63bea7587abd06f5adc988b97cae9e17bf9aaf
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-31 11:01:17 +0700

    cs43 start

---
+++ Cs43/Cs43/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
