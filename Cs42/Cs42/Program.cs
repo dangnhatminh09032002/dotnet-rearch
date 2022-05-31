595a94e01ef017c4915a74a3c04c87cbc4419d34
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-31 08:24:40 +0700

    cs42 start

---
+++ Cs42/Cs42/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cs42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
