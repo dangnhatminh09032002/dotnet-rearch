f8034b607a5177928799329be29398d481a89b9f
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-05-31 10:56:41 +0700

    cs42 Fluent

---
+++ Cs42/Cs42/Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cs42.Domain.Entities
+{
+    //[Table("product")] //Nếu dùng fluent thì không cần phải khai báo điều này nữa
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public void ShowInfo() => Console.WriteLine($"{Id} - {Name} - {Price} - {CategoryId}");
+    }
+}
--- Cs42/Cs42/Program.cs
+++ Cs42/Cs42/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Cs42.Domain.UnitOfWork;
 
 namespace Cs42
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UnitOfWork.DropDatabase();
+            UnitOfWork.CreateDatabase();
+            UnitOfWork.InsertCatogory();
+            UnitOfWork.InsertProduct();
+            UnitOfWork.ShowProduct();
         }
     }
 }
