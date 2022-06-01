df045cc8126c7cce79f3f3cf1419b23c18b7fd15
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-06-01 17:12:45 +0700

    cs50 aspnet razor ep1 20%

---
+++ Razor_Cs50/Razor_Cs50/Startup.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_Cs50
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRazorPages();
+
+            services.AddRazorPages().AddRazorPagesOptions((RazorPagesOptions options) => //AddRazorPagesOptions dùng để cấu hình lại
+            {
+                //options.RootDirectory = "/MyPages"; // Vị trí lưu page (mặc định là /Pages)
+                options.Conventions.AddPageRoute("/Index", ""); // Nếu nhập / thì nó sẽ vào Index.html 
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages(); // Nó sẽ tìm file cshtml trong Pages (đây là folder mặc định)
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
